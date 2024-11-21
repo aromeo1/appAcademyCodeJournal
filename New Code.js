3101b547ad1ca8dc2b698160ea5a28f600dddb63
armond <armondscomputer@armonds-MacBook-Pro.local>
2024-11-20 22:44:06 -0500

    November 20, 2025

---
+++ New Code.js
@@ -0,0 +1,9 @@
+/*
+Wed, Nov 20, 2024
+    Light Binary Trees and Traversal Notes:        
+        -Binary Trees all have a root, and each node has at most two children (left child and right child)
+        -each child node of the root is less then the root node on the left, and greater on the right
+        -breath traversal will visit nodes in the order of the level of the tree
+        -depth traversal will visit nodes in the order of the depth of the tree
+        -need to have sorted Data before using a binary tree to search
+*/
